1e8b6561dfe25d5d486a975ee674a37419030527
davidnavarrete <davidnava1998efrain@outlook.com>
2022-12-13 19:32:16 -0500

    v1

---
+++ dNavarreteS3/dNavarreteS3/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace dNavarreteS3
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public void btnIniciar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            string usuario = "dnavarrete";
+            string contrasena = "12345";
+            if (usuario == txtUsuario.Text && contrasena == txtContrasena.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario/Contraseña Incorrectos", "Cerrar");
+            }
+        }
+    }
+}
+
---
+++ dNavarreteS3/dNavarreteS3/Registro.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace dNavarreteS3
+{
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            
+            InitializeComponent();
+            lblUsuario.Text = usuario; 
+        }
+    }
+}
+
